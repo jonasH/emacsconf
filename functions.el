b49be765ed5733cc445e4d7a957a07ffbcf8ad0c
Jonas Hesselryd <jonas@Mobilaxen.lan>
2016-01-20 21:11:34 +0100

    inital commit

---
+++ functions.el
@@ -0,0 +1,121 @@
+;;; Code:
+(defun super-isearch-forward ()
+  (interactive)
+  (if (region-active-p)
+      (search-selection (region-beginning) (region-end))
+    (isearch-forward)
+  ))
+
+(defun search-selection (beg end)
+      "search for selected text"
+      (let (
+            (selection (buffer-substring-no-properties beg end))
+           )
+        (deactivate-mark)
+        (isearch-mode t nil nil nil)
+        (isearch-yank-string selection)
+      )
+      )
+
+(defun duplicate-line()
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (forward-line 1)
+  (yank)
+  )
+
+(defun duplicate-region ()
+  (interactive)
+  (kill-region (region-beginning) (region-end))
+  (yank)
+  (yank)
+  )
+
+(defun super-duplicate ()
+  (interactive)
+  (if (region-active-p)
+      (duplicate-region)
+    (duplicate-line))
+  )
+
+(defun super-delete ()
+  (interactive)
+  (if (region-active-p)
+      (kill-region (region-beginning) (region-end))
+    (backward-kill-word 1) 
+    ))
+
+
+(defun move-text-internal (arg)
+   (cond
+    ((and mark-active transient-mark-mode)
+     (if (> (point) (mark))
+            (exchange-point-and-mark))
+     (let ((column (current-column))
+              (text (delete-and-extract-region (point) (mark))))
+       (forward-line arg)
+       (move-to-column column t)
+       (set-mark (point))
+       (insert text)
+       (exchange-point-and-mark)
+       (setq deactivate-mark nil)))
+    ((> arg 0) ;; rad ner
+     (beginning-of-line)
+     (forward-line)
+     (transpose-lines 1)
+     (forward-line -1))
+    ((and (< arg 0) (> (line-number-at-pos) 1))
+     (beginning-of-line)
+     (transpose-lines 1)
+     (forward-line -2))
+    ))
+
+
+(defun move-text-down (arg)
+   "Move region (transient-mark-mode active) or current line
+  arg lines down."
+   (interactive "*p")
+   (move-text-internal arg))
+
+(defun move-text-up (arg)
+   "Move region (transient-mark-mode active) or current line
+  arg lines up."
+   (interactive "*p")
+   (move-text-internal (- arg)))
+
+
+(defun revert-all-buffers ()
+    "Refreshes all open buffers from their respective files."
+    (interactive)
+    (dolist (buf (buffer-list))
+      (with-current-buffer buf
+        (when (and (buffer-file-name) (file-exists-p (buffer-file-name)) (not (buffer-modified-p)))
+          (revert-buffer t t t) )))
+    (message "Refreshed open files.") )
+
+
+(defun my/vsplit-last-buffer (prefix)
+  "Split the window vertically and display the previous buffer."
+  (interactive "p")
+  (split-window-vertically)
+  (other-window 1 nil)
+  (if (= prefix 1)
+    (switch-to-next-buffer)))
+
+
+(defun my/hsplit-last-buffer (prefix)
+  "Split the window horizontally and display the previous buffer."
+  (interactive "p")
+  (split-window-horizontally)
+  (other-window 1 nil)
+  (if (= prefix 1) (switch-to-next-buffer)))
+
+
+(defun move-word-backward()
+  (interactive)
+  (transpose-words -1)
+  (backward-word 2)
+  (forward-char))
---
+++ real-init.el
@@ -0,0 +1,85 @@
+;; Put in .emacs (add-hook 'after-init-hook (lambda () (load "~/.emacs.d/elisp/real-init.el")))
+;; Requirements
+;; autopair
+;; flycheck
+;; yasnippet
+;; browse-kill-ring
+;; magit
+(load "~/.emacs.d/elisp/functions.el")
+(load "~/.emacs.d/elisp/highlight-selection.el")
+(highlight-selection-mode 1)
+(add-to-list 'load-path "~/.emacs.d/elisp")
+(autopair-global-mode 1)
+(setq autopair-autowrap 1)
+
+(autoload 'find-file-in-tags "~/.emacs.d/elisp/find-files-in-tags.el")
+(global-set-key (read-kbd-macro "C-,") 'find-file-in-tags)
+
+(global-flycheck-mode 1)
+
+(require 'windmove)
+(windmove-default-keybindings 'meta)
+(require 'linum)
+(global-linum-mode 1)
+
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+
+(global-set-key (kbd "C-x 2") 'my/vsplit-last-buffer)
+(global-set-key (kbd "C-x 3") 'my/hsplit-last-buffer)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+;;magit
+(setq magit-auto-revert-mode nil)
+(setq magit-last-seen-setup-instructions "1.4.0")
+(global-set-key (kbd "M-<f1>") 'magit-status)
+
+(when (require 'browse-kill-ring nil 'noerror)
+  (browse-kill-ring-default-keybindings))
+(global-set-key (kbd "M-y") 'browse-kill-ring)
+
+(yas-global-mode 1)
+
+(global-set-key (kbd "<f12>") 'kill-this-buffer)
+(global-unset-key (kbd "C-v"))
+(global-set-key (kbd "C-v") 'yank)
+(global-set-key (kbd "C-ö") 'super-duplicate)
+(global-set-key (kbd "C-w") 'super-delete)
+(global-set-key (kbd "C-s") 'super-isearch-forward)
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "<f11>") 'mc/edit-lines)
+(global-set-key (kbd "C-<f11>") 'mc/mark-all-like-this)
+(global-set-key (kbd "<f9>") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<f9>") 'mc/skip-to-next-like-this)
+(global-set-key (kbd "<f10>") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-<f10>") 'mc/skip-to-previous-like-this)
+(global-unset-key (kbd "M-<down-mouse-1>"))
+(global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
+(global-set-key (kbd "M-p") 'move-text-up)
+(global-set-key (kbd "M-n") 'move-text-down)
+(global-set-key (kbd "C--") (make-hippie-expand-function
+			     '(try-expand-dabbrev
+			       try-expand-dabbrev-all-buffers) t))
+(global-set-key (kbd "<f2>") 'flycheck-next-error)
+(global-set-key (kbd "<f5>") 'gud-break)
+(global-set-key (kbd "<f6>") 'gud-step)
+(global-set-key (kbd "<f7>") 'gud-next)
+(global-set-key (kbd "<f8>") 'gud-cont)
+
+
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq confirm-nonexistent-file-or-buffer nil)
+(tool-bar-mode -1)
+(column-number-mode 1)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 2) ;; keyboard scroll one line at a time
+(scroll-bar-mode 0)
